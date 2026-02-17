233dfbf23cf33a485fb681595b26d6e9a3082f34
Ranjith478 <vvv9048@srmist.edu.in>
2026-02-17 10:36:19 +0530

    Print a Hardcoded Palindrome Result

--- app/src/Main.java
+++
@@ -1,6 +0,0 @@
-
-void main() {
-    System.out.println("welcome to the palindrome checker management system");
-    System.out.println("Version: 1.0");
-    System.out.println("system initialized successfully.");
-}
